792f313cd9f92dc5ed70f1dbca0f4d31661d56f0
mpuchara <mpuchara@gmail.com>
2026-09-13 20:48:29 +0200

    Wire manual correction static asset

--- adaptive_ai/src/fast_queue_main.py
+++ adaptive_ai/src/fast_queue_main.py
@@ -8,6 +8,7 @@ install_device_targets()
 import queue_main as queued_runtime
 from fast_runtime import install as install_fast_runtime
 from manual_feedback import install as install_manual_feedback
+from manual_feedback_static import install as install_manual_feedback_static
 
 core = queued_runtime.core
 _original_initialize_runtime = core.initialize_runtime
@@ -25,6 +26,7 @@ def initialize_runtime():
 
 core.initialize_runtime = initialize_runtime
 install_manual_feedback(core)
+install_manual_feedback_static(core)
 
 
 if __name__ == "__main__":
