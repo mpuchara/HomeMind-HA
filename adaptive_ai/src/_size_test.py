c496b3ca5b28a01e7b042ac06eae310f1184900e
mpuchara <mpuchara@gmail.com>
2026-09-14 17:25:07 +0200

    Test medium Python write

---
+++ adaptive_ai/src/_size_test.py
@@ -0,0 +1,31 @@
+"""Temporary size test."""
+
+def one(value):
+    return value + 1
+
+def two(value):
+    return one(value) + 1
+
+def three(value):
+    return two(value) + 1
+
+def four(value):
+    return three(value) + 1
+
+def five(value):
+    return four(value) + 1
+
+def six(value):
+    return five(value) + 1
+
+def seven(value):
+    return six(value) + 1
+
+def eight(value):
+    return seven(value) + 1
+
+def nine(value):
+    return eight(value) + 1
+
+def ten(value):
+    return nine(value) + 1
