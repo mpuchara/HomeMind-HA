2581ab6c14d02508d562a9bfa8635807915629a4
mpuchara <mpuchara@gmail.com>
2026-09-20 21:25:41 +0200

    release: bump runtime to 0.14.54

--- adaptive_ai/src/settings.py
+++ adaptive_ai/src/settings.py
@@ -15,7 +15,7 @@ try:
 except Exception:
     ws_connect = None
 
-APP_VERSION = "0.14.53"
+APP_VERSION = "0.14.54"
 HISTORY_BOOTSTRAP_REVISION = "target-attrs-v2"
 TRAINING_REVISION = "shared-home-intents-v18"
 DATA_DIR = Path(os.environ.get("ADAPTIVE_AI_DATA", "/data"))
