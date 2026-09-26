21881a3cba38ea2ef4a87eeed32cfbe3df125562
mpuchara <mpuchara@gmail.com>
2026-09-26 04:32:35 +0200

    Bump version to 0.14.94

--- adaptive_ai/src/settings.py
+++ adaptive_ai/src/settings.py
@@ -15,7 +15,7 @@ try:
 except Exception:
     ws_connect = None
 
-APP_VERSION = "0.14.93"
+APP_VERSION = "0.14.94"
 HISTORY_BOOTSTRAP_REVISION = "target-attrs-v2"
 TRAINING_REVISION = "shared-home-intents-v19"
 DATA_DIR = Path(os.environ.get("ADAPTIVE_AI_DATA", "/data"))
